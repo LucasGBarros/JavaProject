53bd61d0850994f6d589785beaf2b14364484155
Lucas Barros <lucasgomesdebarros99@gmail.com>
2024-10-03 17:30:42 -0300

    Add a New Directory
    New Directory just to challenges

--- src/Challenge.java
+++ src/Challenge.java
@@ -1,4 +1,4 @@
-public class PrimeiraClasse {
+public class Challenge {
     public static void main(String[] args) {
         System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
         System.out.println("Olá, Lucas Barros");
