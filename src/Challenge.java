4102e787c40a6d84ad91fbd46d39913a4fe0a051
Lucas Barros <lucasgomesdebarros99@gmail.com>
2024-10-14 17:26:07 -0300

    Finishing last exercise in class
    Performing the last exercise

--- src/Challenge.java
+++ src/Challenge.java
@@ -115,6 +115,34 @@ public class Challenge {
         System.out.printf("O valor do produto é de R$ %.2f e foi aplicado o desconto de %.0f%%. " +
                         "O valor de desconto foi de R$ %.2f, totalizando R$ %.2f.\n",
                 precoOriginal, percentualDeDesconto, valorDesconto, valorTotalDesconto);
+
+        System.out.println("""
+                              -----------------------------------------------------------------------------------
+                              -----------------------------------  DESAFIO AULA ---------------------------------
+                              -----------------------------------------------------------------------------------""");
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.0;
+        double price2 = 650.00;
+        double measure = 53.264567;
+
+        System.out.printf("""
+                Products:
+                %s, which price is $ %.2f.
+                %s, which price is $ %.2f.
+                
+                Record: %d years old, code %d and gender: %c.
+                
+                Measure with eight decimal places: %f.
+                Rounded (three decimal places): %.3f.
+                US decimal point: %.3f""", product1, price1, product2, price2, age, code, gender,
+                measure, measure, measure);
     }
 }
 
